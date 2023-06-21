8e012246dfb7738a2d0ea9038bf06171859a1e4d
USER <USER@AZVIHUE3688>
2023-06-21 15:30:35 -0600

    Agregar archivos de proyecto.

---
+++ Empower-SUL-MA_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower-SUL-MA_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower-SUL-MA_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("4D47268A-CC90-424E-BA28-CB301976810B")]
+[assembly: AssemblyVersion("1.0.0.0")]
